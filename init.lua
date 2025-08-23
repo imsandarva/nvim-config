ec3181c8d4d60cfb15f08a8dd5adcf8fcd8f87fa
sandarva3 <sandarvapoudel182@gmail.com>
2025-08-23 06:34:06 +0545

    Few changes on main init.lua file

--- init.lua
+++ init.lua
@@ -607,15 +607,16 @@ require('lazy').setup({
           -- code, if the language server you are using supports them
           --
           -- This may be unwanted, since they displace some of your code
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(
-                not vim.lsp.inlay_hint.is_enabled(event.buf), -- check status for this buf
-                { bufnr = event.buf } -- pass bufnr option correctly
-              )
-            end, '[T]oggle Inlay [H]ints')
-          end
-        end,
+if vim.lsp.inlay_hint then
+  if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint) then
+    map('<leader>th', function()
+      vim.lsp.inlay_hint.enable(
+        not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }),
+        { bufnr = event.buf }
+      )
+    end, '[T]oggle Inlay [H]ints')
+  end
+end
       })
 
       -- Diagnostic Config
